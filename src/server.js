22da7b9de3b8e3ef65d2ad74d82122d62142c0f3
Golban Ion <ion.golban322@gmail.com>
2023-04-08 13:23:48 +0300

    up server

--- src/server.js
+++ src/server.js
@@ -1,39 +1,3 @@
-// const http = require('http');
-// const url = require('url');
-// const querystring = require('querystring');
-// const fs = require('fs');
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//     const reqUrl = url.parse(req.url, true);
-//     const reqMethod = req.method;
-//     if (reqMethod === 'GET') {
-//         if (reqUrl.pathname === '/') {
-//             fs.readFile("./html/index.html", function (error, data) {
-//                 if (error) {
-//                     res.writeHead(404);
-//                     res.write('Error: File not found')
-//                 }
-//                 else {
-//                     res.writeHead(202);
-//                     res.write(data)
-//                 }
-//                 res.end()
-//             })
-//         } else if (reqUrl.pathname === '/about') {
-            
-//         } else {
-//             res.writeHead(404, { 'Content-Type': 'text/plain' });
-//             res.end('404 Not Found'); 
-//         }
-//     }
-// })   
-
-// server.listen(port, () => {
-//     console.log('Server running on port 3000');
-// });
-
-
 import http from "http";
 import { servePublicFiles, serveStaticFile } from "./handler.js";
 
